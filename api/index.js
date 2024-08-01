dc432a1f0a4331c6c386380bfb68429f391ae5f8
jhuffm8 <jordan.huffman85@gmail>
2024-08-01 16:58:24 -0700

    error middleware created

--- api/index.js
+++ api/index.js
@@ -14,9 +14,24 @@ mongoose.connect(process.env.MONGO).then(() => {
 const app = express()
 
 app.use(express.json())
+
 app.listen(3000, () => {
     console.log('Listening on port 3000')
 });
 
+
+//api routes
 app.use('/api/user', userRouter);
-app.use('/api/auth', authRouter)
+app.use('/api/auth', authRouter);
+
+
+//middleware to handle errors 
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    });
+})
