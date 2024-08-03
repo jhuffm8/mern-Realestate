b4ea8edf78dd384dc028e53ec29029c7d636aa53
jhuffm8 <jordan.huffman85@gmail>
2024-08-02 17:56:58 -0700

    complete signUp functionality

--- api/index.js
+++ api/index.js
@@ -27,7 +27,7 @@ app.use('/api/auth', authRouter);
 
 //middleware to handle errors 
 app.use((err, req, res, next) => {
-    const statusCode = err.statusCode || 500;
+    const statusCode = err.statusCode || err.status || 500;
     const message = err.message || 'Internal Server Error';
     return res.status(statusCode).json({
         success: false,
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,12 +1,13 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate} from "react-router-dom";
 import axios from "axios";
 
 export default function SignUp() {
 
 const [formData, setFormData] = useState({});
 const [error, setError] = useState(null);
-const [loading, setLoading] = useState(false)
+const [loading, setLoading] = useState(false);
+const navigate = useNavigate()
 
   const handleChange = (e) => {
     setFormData({
@@ -17,13 +18,24 @@ const [loading, setLoading] = useState(false)
 const handleSubmit = async (e) => {
   e.preventDefault();
   try {
+    setLoading(true);
     const res = await axios.post('/api/auth/signup', formData);
-    // do something with the response data
-    console.log(res)
+    console.log(res.data)
+    // if (res.data.success === false){
+    //   setLoading(false)
+    //   setError(res.data.response.data.message)
+    //   return;
+    // }
+
+    setLoading(false);
+    setError(null);
+    navigate('/sign-in');
   } catch (error) {
-    console.error('Error signing up:', error);
-    // you can also display an error message to the user here
+   setLoading(false)
+   setError(error.response.data.message)
   }
+
+
 }
 
   return (
@@ -53,8 +65,8 @@ const handleSubmit = async (e) => {
           id="password"
           onChange={handleChange}
         />
-        <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
-          Sign Up
+        <button disabled={loading} className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
+          {loading ? 'Loading...' : "Sign up"}
         </button>
       </form>
       <div className="flex gap-2 mt-5">
@@ -63,6 +75,7 @@ const handleSubmit = async (e) => {
           <span className="text-blue-200">Sign In</span>
         </Link>
       </div>
+        {error && <p className="text-red-500 mt-5">{error}</p>}
     </div>
   );
 }
